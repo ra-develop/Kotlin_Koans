f86be1cb4890408f707972ecb6db76e14b3d7355
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-26 12:32:39 +0100

    Classes: Task 16/43 finished

--- Conventions/For loop/src/Task.kt
+++ Conventions/For loop/src/Task.kt
@@ -1,4 +1,25 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate): Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object: Iterator<MyDate> {
+
+            var currentDate: MyDate = start
+
+            override fun hasNext(): Boolean = currentDate <= end
+
+            override fun next(): MyDate {
+                val returnDate = currentDate
+                if (returnDate <= end) {
+                    currentDate = returnDate.followingDate()
+                } else {
+                    throw NoSuchElementException()
+                }
+                return returnDate
+            }
+
+        }
+    }
+
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
