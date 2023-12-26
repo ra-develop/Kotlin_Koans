fe834b70d385d1998e48a6ada627d0fdfbde992a
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-26 18:46:45 +0100

    Collections: Task 24/43 finished

--- Collections/GroupBy/src/Task.kt
+++ Collections/GroupBy/src/Task.kt
@@ -1,3 +1,5 @@
 // Build a map that stores the customers living in a given city
 fun Shop.groupCustomersByCity(): Map<City, List<Customer>> =
-        TODO()
+        customers.groupBy {
+                it.city
+        }
