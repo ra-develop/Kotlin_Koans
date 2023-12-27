0bd8273346067b7976f7e71e92ad143786212ee0
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-27 15:29:05 +0100

    Collections: Task 25/43 finished

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,10 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> {
+    val result = customers.filter {customer: Customer ->
+        val (delivered, undelivered) = customer.orders.partition { order: Order ->
+            order.isDelivered
+        }
+        undelivered.size > delivered.size
+    }
+    return result.toSet()
+}
