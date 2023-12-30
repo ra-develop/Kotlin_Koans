faf78071a55d1c3594f7367006a51cd2b1ae9a57
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-30 01:39:00 +0100

    Collections: Task 29/43 finished

--- Collections/Fold and reduce/src/Task.kt
+++ Collections/Fold and reduce/src/Task.kt
@@ -1,5 +1,10 @@
 // Return the set of products that were ordered by all customers
-fun Shop.getProductsOrderedByAll(): Set<Product> =TODO()
+fun Shop.getProductsOrderedByAll(): Set<Product> =
+    customers
+        .map(Customer::getOrderedProducts)
+        .reduce { orderedByAll, customer ->
+            orderedByAll.intersect(customer)
+        }
 
 fun Customer.getOrderedProducts(): Set<Product> =
-    TODO()
+    orders.flatMap(Order::products).toSet()
