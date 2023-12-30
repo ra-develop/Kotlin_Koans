30af4b689e876201284d0fec4b11a2d7792981a0
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-30 15:24:53 +0100

    Collections: Task 30/43 finished

--- Collections/Compound tasks/src/Task.kt
+++ Collections/Compound tasks/src/Task.kt
@@ -1,14 +1,19 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
-fun findMostExpensiveProductBy(customer: Customer): Product? {
-    TODO()
-}
+fun findMostExpensiveProductBy(customer: Customer): Product?
+        = customer.orders.filter(Order::isDelivered)
+        .flatMap(Order::products)
+        .maxByOrNull(Product::price)
+
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
+    return customers.flatMap(Customer::getOrderedProducts)
+        .count {
+            it == product
+        }
 }
 
 fun Customer.getOrderedProducts(): List<Product> =
-        /* TODO */
+    orders.flatMap(Order::products)
